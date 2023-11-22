a915c7bc8496a7dc30d73af98b948fb95457b4e8
Skiller1337 <artemgaame@gmail.com>
2023-11-22 19:20:05 +0200

    Initial commit

---
+++ Controllers/FileController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Text;
+
+public class FileController : Controller
+{
+    private readonly IWebHostEnvironment _hostingEnvironment;
+
+    public FileController(IWebHostEnvironment hostingEnvironment)
+    {
+        _hostingEnvironment = hostingEnvironment;
+    }
+
+    public ActionResult DownloadFile()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult DownloadFile(string firstName, string lastName, string fileName)
+    {
+        if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(fileName))
+        {
+            ModelState.AddModelError("", "Будь ласка, заповніть всі поля форми.");
+            return View();
+        }
+
+        string fileContent = $"Ім'я: {firstName}\r\nПрізвище: {lastName}";
+
+    
+        string folderPath = Path.Combine(_hostingEnvironment.ContentRootPath, "Files");
+        Directory.CreateDirectory(folderPath);
+
+        string filePath = Path.Combine(folderPath, fileName + ".txt");
+
+        System.IO.File.WriteAllText(filePath, fileContent, Encoding.UTF8);
+
+        return File(filePath, "text/plain", fileName + ".txt");
+    }
+}
